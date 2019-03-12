e4bf370773718d7cc7f85f8f2101e55d638c88c9
Günay Gültekin <gunaygultekin@gmail.com>
2019-03-12 23:46:56 +0300

    Add home screen component, update app routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import { ProductCreateComponent } from './product-create/product-create.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'products', component: ProductComponent },
   { path: 'product/create', component: ProductCreateComponent },
   { path: 'cart', component: ShoppingCartComponent },
-  { path: '',   redirectTo: '/products', pathMatch: 'full' }
+  { path: 'home', component: HomeComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,15 @@ import { StoreModule } from '@ngrx/store';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { cartReducer } from '../store/reducers/cart.reducer';
 import { DataSharingService } from '../services/data.service';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductCreateComponent,
     ProductComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
